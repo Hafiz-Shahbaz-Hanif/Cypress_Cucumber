394d741e800847fa72c3739f287f6dfdfbf51cf3
Hafiz Shahbaz Hanif <shahbazhanif87@gmail.com>
2026-08-31 23:35:31 +0500

    feat(ui): login, product sorting, cart and checkout BDD scenarios

---
+++ cypress/e2e/step_definitions/login.steps.js
@@ -0,0 +1,25 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import LoginPage from '../../pages/LoginPage';
+import InventoryPage from '../../pages/InventoryPage';
+
+Given('the login page is open', () => {
+  cy.resetAppState();
+  LoginPage.visit();
+});
+
+When('I sign in as the standard user', () => {
+  LoginPage.login(Cypress.env('standardUser'), Cypress.env('password'));
+});
+
+When('I sign in with username {string} and password {string}', (username, password) => {
+  LoginPage.login(username, password);
+});
+
+Then('the inventory page is shown with {int} products', (count) => {
+  InventoryPage.assertLoaded();
+  InventoryPage.elements.items().should('have.length', count);
+});
+
+Then('I see the login error {string}', (message) => {
+  LoginPage.expectError(message);
+});
